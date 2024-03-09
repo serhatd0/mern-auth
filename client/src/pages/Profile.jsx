c81f19128679ba608c824a0402362bb4072910fc
serhatd0 <demirelserhat14@gmail.com>
2024-03-10 00:33:47 +0300

    update upload image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -41,7 +41,7 @@ export default function Profile() {
       <h1 className='text-3xl font-semibold text-center my-7 '>Profile</h1>
       <form className="flex flex-col gap-4">
         <input type="file" ref={fileRef} hidden accept='image/*' onChange={(e)=>setImage(e.target.files[0])}/>
-        <img src={currentUser.profilePicture} className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2" alt='profile' onClick={()=>fileRef.current.click()}/>
+        <img src={formData.profilePicture || currentUser.profilePicture} className="h-24 w-24 self-center cursor-pointer rounded-full object-cover mt-2" alt='profile' onClick={()=>fileRef.current.click()}/>
         <p className="text-sm self-center">
           {imageError ? 
           (<span className="text-red-700">Error Uploading image(file size must be less than 2 MB )</span>) :
